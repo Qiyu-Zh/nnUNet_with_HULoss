b110fe0d1a1f31eeae242d672672a4706cbdb51c
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-20 15:51:43 -0700

    Update compound_losses.py

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -31,7 +31,6 @@ class DC_and_CE_loss(nn.Module):
         self.dc = dice_class(apply_nonlin=softmax_helper_dim1, **soft_dice_kwargs)
         if hd:
             self.hd = HD_loss(apply_nonlin=softmax_helper_dim1, alpha = 2)
-            print("---------------------------------------------------------HD_loss gpu is applied-----------------------------------------------------------------")
         else:
             self.hd = None
 
